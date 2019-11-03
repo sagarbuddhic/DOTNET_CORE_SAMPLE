343e1f8ea6bfd5592354e3b66766a8ee6ffa11ba
sagarbuddhic <sagar.buddhi@domain.com.au>
2019-11-03 18:49:59 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DOTNET_CORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string arg in args) {
+                Console.WriteLine($"Hello {arg}");
+            }
+        }
+    }
+}
